d7831c2c8cf7e135ffd09e5bfa00fe6e78b815c1
Aðalsteinn Rúnarsson <allir@wuxinextcode.com>
2019-01-03 13:08:41 +0000

    Write back the request

--- main.go
+++ main.go
@@ -5,29 +5,35 @@ import(
 	"log"
 	"fmt"
 	"net/http"
+	"net/http/httputil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+  hostname, err := os.Hostname()
+  if err != nil {
+    log.Fatal(err)
+  }
 	
-	hostname, err := os.Hostname()
-	if err != nil {
-    panic(err)
-	}
-	
-	title := os.Getenv("HELLO_VAR")
+  title := os.Getenv("HELLO_VAR")
   if title == "" {
     title = "allir"
   }
 	
-  log.Println("Writing response with title = " + title)
-	fmt.Fprintf(w, "Hello from " + title + "\nYou've requested %s\n\n\n\n\nServing from " + hostname, r.URL.Path) 
-
+  reqDump, err := httputil.DumpRequest(r, true)
+  if err != nil {
+    log.Fatal(err)
+  }
+	
+	log.Println(r.URL.Path + " -  Writing response with title = " + title)
+  fmt.Fprintf(w, "Hello from %s\n\n%s\n\n\n\n\nServing from %s", title, string(reqDump), hostname)
 }
+
+func handlerFavIcon(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
   log.Println("Starting server, listening on port 8080")
 	
-  http.HandleFunc("/", handler)
-	log.Fatal( http.ListenAndServe(":8080", nil) )
+	http.HandleFunc("/favicon.ico", handlerFavIcon)
+	http.HandleFunc("/", handler)
+  log.Fatal( http.ListenAndServe(":8080", nil) )
 }
-
